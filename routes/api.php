fd8c210de14a5efb602c9d7967e48ab77e3e43f8
unknown <afgarcia0479@misena.edu.co>
2018-12-08 19:21:16 -0500

    Created Model, Controller, Route with version

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,32 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+// V1 API 
+Route::group(['prefix' => 'v1'], function () {
+    // Route::apiResource('categories', 'CategoryController@getCategory', ['only' => ['index', 'store', 'update', 'destroy', 'show']]);
+
+    Route::apiResource('restaurants', 'RestaurantController');
+});
+// END V1 API 
+
+
+// V2 API
+Route::group(['prefix' => 'v2'], function () {
+    // Route::apiResource('categories-profile', 'CategoryController@getUser');
+    // Route::get('users/names', 'UserController@names');
+
+	// Category
+    Route::apiResource('categories', 'CategoryController');
+    Route::apiResource('categories/names', 'CategoryController@names');
+	Route::group(['prefix'=>'categories'],function(){
+		Route::apiResource('/{category}/restaurants','RestaurantController');
+	});
+    
+    // Restaurant
+    Route::apiResource('restaurants', 'RestaurantController');
+    Route::apiResource('restaurants/names', 'RestaurantController@names');
+
+});
+// END V2 API
