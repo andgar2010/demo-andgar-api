d6fa4656b2cc48d7f21a2f323a81199c88e01105
unknown <afgarcia0479@misena.edu.co>
2018-12-08 20:01:42 -0500

    Relationship Created

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,9 +2,13 @@
 
 namespace App\Models;
 
+use App\Models\Restaurant;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    public function restaurants()
+    {
+    	return $this->hasMany(Restaurant::class);
+    }
 }
